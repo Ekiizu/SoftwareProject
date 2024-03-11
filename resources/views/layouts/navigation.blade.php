1f253b44258ffce1cb04e5e29539d36dd9d90243
Ekiizu <ekiizu2003@gmail.com>
2024-03-11 16:52:44 +0000

    added bootstrap

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,4 +1,4 @@
-<nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
+<nav x-data="{ open: false }" class="bg-primary text-white border-b border-gray-100">
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
@@ -6,7 +6,8 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
+                        <img src="{{ asset('image/bugcat.gif') }}" alt="logo" class="block h-9 w-auto">
+                        {{-- need to fix --}}
                     </a>
                 </div>
 
